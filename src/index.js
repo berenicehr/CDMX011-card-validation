d22b9970008adf49f2e402cb8613d728f355b1ab
Berenice Huerta <bere4758@gmail.com>
2021-07-07 11:57:28 -0500

    maskify resuelto en testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+/* eslint-disable no-console */
+
+
+
 import validator from './validator.js';
 
-console.log(validator);
+
+document.getElementById("button").addEventListener("click", function()
+{
+    const registroTarjeta = document.getElementById("cardNumber").value;
+    const result= validator.isValid(registroTarjeta)
+    console.log(result)
+    const hideNumbers = validator.maskify(registroTarjeta)
+    console.log(hideNumbers)
+}
+);
+   
