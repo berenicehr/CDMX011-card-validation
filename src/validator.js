fd6ca54bb7711c372fc20382ae763a86ef80b8cc
Berenice Huerta <bere4758@gmail.com>
2021-07-14 12:42:35 -0500

    primer commit con avances css,html,js

--- src/validator.js
+++ src/validator.js
@@ -1,65 +1,64 @@
 /* eslint-disable no-console */
 
-const validator = { 
+const validator = {
 
-  "isValid": function(numerosIniciales){
+  "isValid": function(numerosIniciales) {
     /*construir un array de los números de tarjeta*/
-  const arreglo=Array.from(numerosIniciales).map(Number);
+    const arreglo = Array.from(numerosIniciales).map(Number);
  
-  /*invertir los numeros del array*/
-  const reversa=arreglo;
-  console.log(reversa);  
+    /*invertir los numeros del array*/
+    const reversa = arreglo.reverse();
+    console.log(reversa);  
   
-  /*Obtener los números pares para multiplicarlos por dos */
-  let suma = 0;
-  for (let i=0; i<reversa.length; i++){
-   
-    if (i % 2 == 0){ 
-      let multiplicar = reversa[i]*2;
-      console.log(multiplicar);
-      if (multiplicar>=10){
-        let numeroFinal= multiplicar-9;
-        suma = suma+numeroFinal;
+    /*Obtener los números pares para multiplicarlos por dos */
+    let suma = 0;
+  
+    /*if (reversa.length == 16){}*/{
+      for (let i = 0; i < reversa.length; i++) {
+        if (i % 2 != 0) { 
+          let multiplicar = reversa[i]*2;
+          console.log(multiplicar)
+          if (multiplicar >= 10) {
+            let numeroFinal = multiplicar-9;
+            suma = suma+numeroFinal;
+          }
+          else {
+            suma = suma+multiplicar;
+            console.log(suma);
+          }  
+        }
+        else {
+          suma = suma+reversa[i];
+          console.log(suma);
+        }
+      }  
+
+      console.log(suma);
+      let resultado = suma % 10;
+      if (resultado == 0) {
+        return  true;   
       }
-      else{
-        suma = suma+multiplicar;
-        console.log(suma);
+      else {
+        return false;
       }
-    }
-    else {
-        suma = suma+reversa[i];
-        console.log(suma);
+      
     }
   
-  }
-
-  console.log(suma);
-  let resultado = suma % 10;
-  if (resultado == 0) {
-    return  true; 
-  }
-  else {
-    return false;
-  }
-
+    // eslint-disable-next-line no-unreachable
+    
+  },
   
-
-  // eslint-disable-next-line no-unreachable
- /* return {numerosIniciales}*/
-},
-
-  
-  "maskify": function(hidden){ 
-    let longitud= hidden.length;
-    if(longitud<=4){
+  "maskify": function(hidden) { 
+    let longitud = hidden.length;
+    if(longitud <= 4) {
       return (hidden);
     }
     else {
       let listaCadena=Array.from(hidden);
-      for (let i=0; i<hidden.length-4; i++){
-        listaCadena[i]="#";
+      for (let i = 0; i<hidden.length-4; i++) {
+        listaCadena[i] = "#";
       }
-      let revision=listaCadena.join("");
+      let revision = listaCadena.join("");
       return revision;
     }  
   }
